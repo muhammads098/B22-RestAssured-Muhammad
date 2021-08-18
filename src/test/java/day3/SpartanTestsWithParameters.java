54f1b2dc6aafe78a18e512517100b6702cc5ca1d
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-08-17 20:41:46 -0400

    day3

--- src/test/java/day3/SpartanTestsWithParameters.java
+++ src/test/java/day3/SpartanTestsWithParameters.java
@@ -18,7 +18,7 @@ public class SpartanTestsWithParameters {
     @BeforeAll
     public static void init(){
         //save baseurl inside this variable so that we dont need to type each http method.
-        baseURI = "http://52.207.61.129:8000";
+        baseURI = "http://34.207.125.3:8000";
     }
 
      /*   Given accept type is Json
