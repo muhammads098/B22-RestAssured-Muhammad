6d49c9e5d19fbd8e49bfc71d282ceaf79e56bbc0
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-08-17 21:20:55 -0400

    day3

--- src/test/java/day3/ORDSApiTestsWithParameters.java
+++ src/test/java/day3/ORDSApiTestsWithParameters.java
@@ -1,7 +1,11 @@
 package day3;
 
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,5 +25,41 @@ public class ORDSApiTestsWithParameters {
         And Content type is application/json
         And Payload should contain "United States of America"
      */
+
+    @DisplayName("Get request to /countries with Query Param")
+    @Test
+    public void test1(){
+        Response response= given().accept(ContentType.JSON)
+                .and().queryParam("q","{\"region_id\":2}")
+                .log().all()
+                .when()
+                .get("/countries");
+
+        assertEquals(200,response.statusCode());
+        assertEquals("application/json",response.header("Content-Type"));
+        assertTrue(response.body().asString().contains("United States of America"));
+
+        response.prettyPrint();
+
+    }
+    /*
+        Send a GET request to employees and get only employees who works as a IT_PROG
+     */
+
+    @DisplayName("GET request to /employees with Query Param")
+    @Test
+    public void test2(){
+        Response response= given().accept(ContentType.JSON)
+                .and().queryParam("q","{\"job_id\": \"IT_PROG\"}")
+                .when().get("/employees");
+
+        assertEquals(200,response.statusCode());
+        assertEquals("application/json",response.header("Content-Type"));
+        assertTrue(response.body().asString().contains("IT_PROG"));
+
+        response.prettyPrint();
+
+    }
+
 
 }
