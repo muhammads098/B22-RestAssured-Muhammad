7e4940479d8f5586c564cf852b55305a942c7985
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-08-17 20:00:11 -0400

    day3:pathParam test3

--- src/test/java/day3/SpartanTestsWithParameters.java
+++ src/test/java/day3/SpartanTestsWithParameters.java
@@ -41,17 +41,69 @@ public class SpartanTestsWithParameters {
 
 
     }
-    @DisplayName("GET request to /api/spartans/{id} with ID 500")
+     /*
+        TASK
+        Given accept type is Json
+        And Id parameter value is 500
+        When user sends GET request to /api/spartans/{id}
+        Then response status code should be 404
+        And response content-type: application/json
+        And "Not Found" message should be in response payload
+     */
+
+    @DisplayName("GET request /api/spartans/{id} Negative Test")
     @Test
     public void test2(){
-        Response response = given().accept(ContentType.JSON).and().pathParam("id",500).when().get("/api/spartans/{id}");
+
+        Response response = given().accept(ContentType.JSON).log().all()
+                .pathParam("id", 500)
+                .when()
+                .get("/api/spartans/{id}");
+
+        //verify status code 404
         assertEquals(404,response.statusCode());
+        //verify content type
         assertEquals("application/json",response.contentType());
+        //verify NotFound in the json payload/body
         assertTrue(response.body().asString().contains("Not Found"));
 
+    }
 
+     /*
+        Given accept type is Json
+        And query parameter values are:
+        gender|Female
+        nameContains|e
+        When user sends GET request to /api/spartans/search
+        Then response status code should be 200
+        And response content-type: application/json
+        And "Female" should be in response payload
+        And "Janette" should be in response payload
+     */
+
+    @DisplayName("GET request to /api/spartans/search with Query Params")
+    @Test
+    public void test3(){
+        Response response= given().log().all().
+                accept(ContentType.JSON)
+                .and().queryParam("nameContains","e")
+                .and().queryParam("gender","Female")
+                .when()
+                .get("/api/spartans/search");
+
+        //verify status code 200
+        assertEquals(200,response.statusCode());
+        //verify content type
+        assertEquals("application/json",response.contentType());
+        //verify NotFound in the json payload/body
+
+        //"Female" should be in response payload
+        assertTrue(response.body().asString().contains("Female"));
+        //"Janette" should be in response payload
+        assertTrue(response.body().asString().contains("Janette"));
 
     }
+
 
 
 }
