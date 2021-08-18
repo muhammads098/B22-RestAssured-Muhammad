b2934e84db90c53e1f7e20d21f2cc0b42b34e475
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-08-17 21:52:57 -0400

    day3

--- src/test/java/day3/SpartanTestsWithPath.java
+++ src/test/java/day3/SpartanTestsWithPath.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.baseURI;
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -19,17 +21,17 @@ public class SpartanTestsWithPath {
     }
 
     /*
-     Given accept type is json
-     And path param id is 10
-     When user sends a get request to "api/spartans/{id}"
-     Then status code is 200
-     And content-type is "application/json"
-     And response payload values match the following:
-          id is 10,
-          name is "Lorenza",
-          gender is "Female",
-          phone is 3312820936
-   */
+      Given accept type is json
+      And path param id is 10
+      When user sends a get request to "api/spartans/{id}"
+      Then status code is 200
+      And content-type is "application/json"
+      And response payload values match the following:
+           id is 10,
+           name is "Lorenza",
+           gender is "Female",
+           phone is 3312820936
+    */
     @DisplayName("GET one spartan with Path Method")
     @Test
     public void test1(){
@@ -63,5 +65,31 @@ public class SpartanTestsWithPath {
         assertEquals(3312820936l,phone);
 
 
+    }
+
+    @DisplayName("GET all spartan and navigate with Path()")
+    @Test
+    public void test2(){
+        Response response = given().accept(ContentType.JSON)
+                .when().get("/api/spartans");
+
+        //response.prettyPrint();
+
+        int firstId = response.path("id[0]");
+        System.out.println("firstId = " + firstId);
+
+        String name = response.path("name[0]");
+        System.out.println("name = " + name);
+
+        String lastFirstName = response.path("name[-2]");
+        System.out.println("lastFirstName = " + lastFirstName);
+
+        //save names inside the list of string
+        List<String> names = response.path("name");
+        System.out.println(names);
+        //print each name one by one
+        for (String n : names) {
+            System.out.println(n);
+        }
     }
 }
