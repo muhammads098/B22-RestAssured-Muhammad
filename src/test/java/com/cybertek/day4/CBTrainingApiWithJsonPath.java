4c3ee35dfd6f896394ca28303ad29061ceb2c784
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-08-18 22:10:57 -0400

    day4

---
+++ src/test/java/com/cybertek/day4/CBTrainingApiWithJsonPath.java
@@ -0,0 +1,46 @@
+package com.cybertek.day4;
+
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.baseURI;
+import static io.restassured.RestAssured.given;
+
+public class CBTrainingApiWithJsonPath {
+
+    @BeforeAll
+    public static void init(){
+        //save baseurl inside this variable so that we dont need to type each http method.
+        baseURI = "http://api.cybertektraining.com";
+
+    }
+
+
+    @DisplayName("GET Request to individual student")
+    @Test
+    public void test1(){
+        //send a get request to student id 23401 as a path parameter and accept header application/json
+        //verify status code=200 /content type=application/json;charset=UTF-8 /Content-Encoding = gzip
+        //verify Date header exists
+        //assert that
+            /*
+                firstName Vera
+                batch 14
+                section 12
+                emailAddress aaa@gmail.com
+                companyName Cybertek
+                state IL
+                zipCode 60606
+                using JsonPath
+             */
+
+
+
+
+
+
+    }
+}
--- src/test/java/com/cybertek/day4/ORDSApiWithJsonPath.java
+++ src/test/java/com/cybertek/day4/ORDSApiWithJsonPath.java
@@ -41,7 +41,34 @@ public class ORDSApiWithJsonPath extends HRTestBase {
         List<String> countryNameWithRegionId2 = jsonPath.getList("items.findAll {it.region_id==2}.country_name");
         System.out.println(countryNameWithRegionId2);
 
+    }
+
+       @DisplayName("GET requesto /employees with query param")
+       @Test
+        public void test2(){
+        //we added limit query param to get 107 employees
+        Response response = given().queryParam("limit", 107)
+                .when().get("/employees");
+
+        JsonPath jsonPath = response.jsonPath();
+
+        //get me all email of employees who is working as IT_PROG
+        List<String> employeeITProgs = jsonPath.getList("items.findAll {it.job_id==\"IT_PROG\"}.email");
+        System.out.println(employeeITProgs);
+
+
+
+           //get me first name of employees who is making more than 10000
+           List<String> empNames = jsonPath.getList("items.findAll {it.salary>10000}.first_name");
+           System.out.println(empNames);
+
+           //get the max salary first_name
+           String kingFirstName = jsonPath.getString("items.max {it.salary}.first_name");
+           String kingNameWithPathMethod = response.path("items.max {it.salary}.first_name");
+           System.out.println("kingFirstName = " + kingFirstName);
+           System.out.println("kingNameWithPathMethod = " + kingNameWithPathMethod);
 
 
     }
+
 }
