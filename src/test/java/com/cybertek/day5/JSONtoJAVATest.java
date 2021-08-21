04bcd3f922f409f86ef0e861b79508c6cff48611
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-08-21 15:44:45 -0400

    day5

--- src/test/java/com/cybertek/day5/JSONtoJAVATest.java
+++ src/test/java/com/cybertek/day5/JSONtoJAVATest.java
@@ -5,6 +5,7 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
@@ -12,11 +13,32 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 public class JSONtoJAVATest extends SpartanTestBase {
+    @DisplayName("GET one Spartan and deserialize to Map")
     @Test
     public void oneSpartanToMap(){
-       Response response =  given().pathParam("id",15).when().get("/api/spartans/{id}").then()
-               .statusCode(200).extract().response();
 
-        Map<String,Object>jsonMap = response.as(Map.class);
+        Response response = given().pathParam("id", 15)
+                .when().get("/api/spartans/{id}")
+                .then().statusCode(200).extract().response();
+
+        //get the json and convert it to the map
+
+        Map<String,Object> jsonMap = response.as(Map.class);
+
+
+        System.out.println(jsonMap.toString());
+        //after we got the map, we can use hamcrest or junit assertions to do assertion
+        String actualName = (String) jsonMap.get("name");
+        assertThat(actualName,is("Meta"));
     }
+
+    @Test
+    public void test2(){
+        Response response = get("/api/spartans")
+                .then().statusCode(200).extract().response();
+        List<Map<String,Object>> allSpartans=response.as(List.class);
+
+    }
+
+
 }
