a19817fc9cd62fd9b32a6a8934534b671973a00f
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-08-22 12:50:23 -0400

    day6

---
+++ src/test/java/com/cybertek/day6/ORDSPojoGetRequestTest.java
@@ -0,0 +1,32 @@
+package com.cybertek.day6;
+
+import com.cybertek.pojo.Link;
+import com.cybertek.pojo.Region;
+import com.cybertek.utilities.HRTestBase;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+public class ORDSPojoGetRequestTest extends HRTestBase {
+
+    @Test
+    public void regionTest(){
+
+        JsonPath jsonPath = get("/regions").then().statusCode(200).extract().jsonPath();
+
+        Region region1 = jsonPath.getObject("items[0]", Region.class);
+
+        System.out.println(region1);
+
+        System.out.println("region1.getRegion_id() = " + region1.getRegion_id());
+        System.out.println("region1.getRegion_name() = " + region1.getRegion_name());
+        System.out.println("region1.getLinks().get(0).getHref() = " + region1.getLinks().get(0).getHref());
+
+
+
+    }
+}
---
+++ src/test/java/com/cybertek/pojo/Region.java
@@ -0,0 +1,17 @@
+package com.cybertek.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+@Getter
+@Setter
+@ToString
+
+public class Region {
+    private int region_id;
+    private String region_name;
+    private List<Link> links;
+
+}
