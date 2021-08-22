4a89f2eab4df230d45481597f551c45ae1c161b8
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-08-22 11:21:03 -0400

    day6 spartan class

--- src/test/java/com/cybertek/day6/SpartanPojoGetRequestTest.java
+++ src/test/java/com/cybertek/day6/SpartanPojoGetRequestTest.java
@@ -3,6 +3,7 @@ package com.cybertek.day6;
 import com.cybertek.pojo.Spartan;
 import com.cybertek.utilities.SpartanTestBase;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -25,11 +26,22 @@ public class SpartanPojoGetRequestTest extends SpartanTestBase {
         //De serialize --> JSON to POJO (java custom class)
         //2 different way to do this
         //1.using as() method
+        //we convert json response to spartan object with the help of jackson
+        //as() method uses jackson to de serialize(converting JSON to Java class)
         Spartan spartan15 = response.as(Spartan.class);
         System.out.println(spartan15);
         System.out.println("spartan15.getId() = " + spartan15.getId());
         System.out.println("spartan15.getGender() = " + spartan15.getGender());
 
+        //second way of deserialize json to java
+        //2.using JsonPath to deserialize to custom class
+        JsonPath jsonPath = response.jsonPath();
 
+        Spartan s15 = jsonPath.getObject("", Spartan.class);
+
+
+        System.out.println(s15);
+        System.out.println("s15.getName() = " + s15.getName());
+        System.out.println("s15.getPhone() = " + s15.getPhone());
     }
 }
