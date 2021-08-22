0adb13e3734f2529e61c595a817d533a8c3245d8
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-08-22 15:45:37 -0400

    day6

--- src/test/java/com/cybertek/day6/ORDSPojoGetRequestTest.java
+++ src/test/java/com/cybertek/day6/ORDSPojoGetRequestTest.java
@@ -3,11 +3,14 @@ package com.cybertek.day6;
 import com.cybertek.pojo.Employee;
 import com.cybertek.pojo.Link;
 import com.cybertek.pojo.Region;
+import com.cybertek.pojo.Regions;
 import com.cybertek.utilities.HRTestBase;
 import io.restassured.path.json.JsonPath;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static io.restassured.RestAssured.*;
@@ -16,7 +19,7 @@ import static org.hamcrest.Matchers.*;
 public class ORDSPojoGetRequestTest extends HRTestBase {
 
     @Test
-    public void regionTest(){
+    public void regionTest() {
 
         JsonPath jsonPath = get("/regions").then().statusCode(200).log().body().extract().jsonPath();
 
@@ -32,7 +35,7 @@ public class ORDSPojoGetRequestTest extends HRTestBase {
 
     @DisplayName("GET request to /employees and only get couple of values as a Pojo class")
     @Test
-    public void employeeGet(){
+    public void employeeGet() {
 
         Employee employee1 = get("/employees").then().statusCode(200)
                 .extract().jsonPath().getObject("items[0]", Employee.class);
@@ -40,4 +43,37 @@ public class ORDSPojoGetRequestTest extends HRTestBase {
         System.out.println(employee1);
 
     }
+
+    @DisplayName("GET request to region only some fields test")
+    @Test
+    public void regionPojoTest(){
+        //send a get request and save everthing inside the regions object
+        //since we prepare pojo also for the all properties we dont need to use any path so as() method is enough
+        Regions regions = get("/regions").then().statusCode(200).extract().response().as(Regions.class);
+
+        //verify count is 4
+        assertThat(regions.getCount(),is(4));
+        //create empty list to store values
+        List<String> regionNames = new ArrayList<>();
+        List<Integer> regionIds = new ArrayList<>();
+
+        //get list of regions out of regions object
+        List<Region> items = regions.getItems();
+
+        //loop through each of the region, save their ids and names to empty list that we prepare
+        for (Region region : items) {
+            regionIds.add(region.getRId());
+            regionNames.add(region.getRegion_name());
+        }
+        System.out.println("regionIds = " + regionIds);
+        System.out.println("regionNames = " + regionNames);
+        //prepare expected result
+        List<Integer> expectedRegionIds = Arrays.asList(1,2,3,4);
+        List<String> expectedRegionNames = Arrays.asList("Europe", "Americas", "Asia", "Middle East and Africa");
+
+        //compare two result
+        assertThat(regionIds,is(expectedRegionIds));
+        assertThat(regionNames,is(equalTo(expectedRegionNames)));
+    }
+
 }
--- src/test/java/com/cybertek/pojo/Region.java
+++ src/test/java/com/cybertek/pojo/Region.java
@@ -1,5 +1,6 @@
 package com.cybertek.pojo;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +11,7 @@ import java.util.List;
 @Getter
 @Setter
 @ToString
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Region {
     //region_id
     //if your jsonkey and variable name not matching, you can map it with jsonProperty
