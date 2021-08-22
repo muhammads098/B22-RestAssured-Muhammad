8a45984eaf7f5785eed686b1e8a114f767fc0f1a
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-08-22 14:52:41 -0400

    day6

--- src/test/java/com/cybertek/day6/ORDSPojoGetRequestTest.java
+++ src/test/java/com/cybertek/day6/ORDSPojoGetRequestTest.java
@@ -1,9 +1,11 @@
 package com.cybertek.day6;
 
+import com.cybertek.pojo.Employee;
 import com.cybertek.pojo.Link;
 import com.cybertek.pojo.Region;
 import com.cybertek.utilities.HRTestBase;
 import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
@@ -16,17 +18,26 @@ public class ORDSPojoGetRequestTest extends HRTestBase {
     @Test
     public void regionTest(){
 
-        JsonPath jsonPath = get("/regions").then().statusCode(200).extract().jsonPath();
+        JsonPath jsonPath = get("/regions").then().statusCode(200).log().body().extract().jsonPath();
 
         Region region1 = jsonPath.getObject("items[0]", Region.class);
 
         System.out.println(region1);
 
-        System.out.println("region1.getRegion_id() = " + region1.getRegion_id());
+        System.out.println("region1.getRegion_id() = " + region1.getRId());
         System.out.println("region1.getRegion_name() = " + region1.getRegion_name());
         System.out.println("region1.getLinks().get(0).getHref() = " + region1.getLinks().get(0).getHref());
 
+    }
 
+    @DisplayName("GET request to /employees and only get couple of values as a Pojo class")
+    @Test
+    public void employeeGet(){
+
+        Employee employee1 = get("/employees").then().statusCode(200)
+                .extract().jsonPath().getObject("items[0]", Employee.class);
+
+        System.out.println(employee1);
 
     }
 }
--- src/test/java/com/cybertek/pojo/Region.java
+++ src/test/java/com/cybertek/pojo/Region.java
@@ -1,17 +1,23 @@
 package com.cybertek.pojo;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.util.List;
+
 @Getter
 @Setter
 @ToString
-
 public class Region {
-    private int region_id;
+    //region_id
+    //if your jsonkey and variable name not matching, you can map it with jsonProperty
+    @JsonProperty("region_id")
+    private int rId;
+    @JsonProperty("region_name")
     private String region_name;
+    @JsonProperty("links")
     private List<Link> links;
 
 }
