1f31d754d03c43c42845570b8e00e08bd8edcc6a
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-08-24 20:18:55 -0400

    day7 2ways to post automation

---
+++ src/test/java/com/cybertek/day7/SpartanPostRequestDemo.java
@@ -0,0 +1,89 @@
+package com.cybertek.day7;
+
+import com.cybertek.utilities.SpartanTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+public class SpartanPostRequestDemo extends SpartanTestBase {
+
+    /*
+ Given accept type and Content type is JSON
+ And request json body is:
+ {
+   "gender":"Male",
+   "name":"Severus",
+   "phone":8877445596
+}
+ When user sends POST request to '/api/spartans'
+ Then status code 201
+ And content type should be application/json
+ And json payload/response/body should contain:
+ "A Spartan is Born!" message
+ and same data what is posted
+*/
+    @Test
+    public void postMethod1(){
+
+        String requestJsonBody = "{\"gender\":\"Male\",\n" +
+                "\"name\":\"Severus\",\n" +
+                "\"phone\":8877445596}";
+
+        Response response = given().accept(ContentType.JSON).and()
+                .contentType(ContentType.JSON)
+                .body(requestJsonBody)
+                .when()
+                .post("/api/spartans");
+
+        //verify status code
+        assertThat(response.statusCode(),is(201));
+        assertThat(response.contentType(),is("application/json"));
+
+        String expectedResponseMessage = "A Spartan is Born!";
+        assertThat(response.path("success"),is(expectedResponseMessage));
+        assertThat(response.path("data.name"),is("Severus"));
+        assertThat(response.path("data.gender"),is("Male"));
+        assertThat(response.path("data.phone"),is(8877445596L));
+
+
+    }
+
+    @DisplayName("POST with Map to JSON")
+    @Test
+    public void postMethod2(){
+
+        //create a map to keep request json body information
+        Map<String,Object> requestJsonMap = new LinkedHashMap<>();
+        requestJsonMap.put("name","Severus");
+        requestJsonMap.put("gender","Male");
+        requestJsonMap.put("phone",8877445596L);
+
+
+        Response response = given().accept(ContentType.JSON).and() //what we are asking from api which is JSON response
+                .contentType(ContentType.JSON) //what we are sending to api, which is JSON also
+                .body(requestJsonMap).log().all()
+                .when()
+                .post("/api/spartans");
+
+        //verify status code
+        assertThat(response.statusCode(),is(201));
+        assertThat(response.contentType(),is("application/json"));
+
+        String expectedResponseMessage = "A Spartan is Born!";
+        assertThat(response.path("success"),is(expectedResponseMessage));
+        assertThat(response.path("data.name"),is("Severus"));
+        assertThat(response.path("data.gender"),is("Male"));
+        assertThat(response.path("data.phone"),is(8877445596L));
+
+        response.prettyPrint();
+    }
+
+
+}
