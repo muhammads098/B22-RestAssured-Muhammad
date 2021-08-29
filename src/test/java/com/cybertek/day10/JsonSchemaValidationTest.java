9f78d4d84f953df4bae87e7f004658c6a3281768
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-08-29 13:01:02 -0400

    day10

---
+++ src/test/java/com/cybertek/day10/JsonSchemaValidationTest.java
@@ -0,0 +1,33 @@
+package com.cybertek.day10;
+
+import com.cybertek.utilities.SpartanAuthTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.module.jsv.JsonSchemaValidator;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+public class JsonSchemaValidationTest extends SpartanAuthTestBase {
+
+    @DisplayName("GET request to verify one spartan against to schema")
+    @Test
+    public void schemaValidation(){
+
+        given()
+                .accept(ContentType.JSON)
+                .and()
+                .pathParam("id",117)
+                .and()
+                .auth().basic("admin","admin")
+                .when()
+                .get("/api/spartans/{id}")
+                .then()
+                .statusCode(200)
+                .body(JsonSchemaValidator.matchesJsonSchemaInClasspath("SingleSpartanSchema.json"))
+                .log().all();
+
+    }
+
+}
