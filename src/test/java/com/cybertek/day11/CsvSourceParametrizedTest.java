721c7045be5c50f56fb945a6d1871d95f05fd6b5
muhammad shoaib <muhammadshoaib280@gmail.com>
2021-08-31 20:17:30 -0400

    day11

---
+++ src/test/java/com/cybertek/day11/CsvSourceParametrizedTest.java
@@ -0,0 +1,24 @@
+package com.cybertek.day11;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class CsvSourceParametrizedTest {
+
+    // Test first number + second number = third number
+//            1, 3 , 4
+//            2, 3 , 5
+//            8, 7 , 15
+//            10, 9 , 19
+
+    @ParameterizedTest
+    @CsvSource({"1, 3 , 4",
+            "2, 3 , 5",
+            "8, 7 , 15",
+            "10, 9 , 19"})
+    public void testAddition(int num1,int num2, int sum){
+
+
+    }
+
+}
